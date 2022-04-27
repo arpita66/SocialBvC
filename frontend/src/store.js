443054a0625e36406d3e8aae70bb410d8861425c
Arpita <arpipandey66@gmail.com>
2022-04-27 10:15:56 +0530

    .

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {
-//    allUsersReducer,
-//    postOfFollowingReducer,
-//    userProfileReducer,
+   allUsersReducer,
+    postOfFollowingReducer,
+    userProfileReducer,
     userReducer,
   } from "./Reducers/User";
-// import { likeReducer, myPostsReducer, userPostsReducer } from "./Reducers/Post";
+ import { likeReducer, myPostsReducer, userPostsReducer } from "./Reducers/Post";
 const store = configureStore({
     reducer: {
       user: userReducer,
-      //postOfFollowing: postOfFollowingReducer,
-      //allUsers: allUsersReducer,
-      //like: likeReducer,
-      //myPosts: myPostsReducer,
-      //userProfile: userProfileReducer,
-      //userPosts: userPostsReducer,
+      postOfFollowing: postOfFollowingReducer,
+      allUsers: allUsersReducer,
+      like: likeReducer,
+      myPosts: myPostsReducer,
+      userProfile: userProfileReducer,
+      userPosts: userPostsReducer,
     },
   });
 export default store;
