c15d60294caf454a0d0e0f719a701f4e01bd4fcf
Apurva1402 <90923017+Apurva1402@users.noreply.github.com>
2022-04-29 16:12:20 +0530

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Login from "./Components/Login/Login";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { loadUser } from "./Actions/User";
-import Login from "./Components/Login/Login";
+import Home from "./Components/Home/Home";
+import Account from "./Components/Account/Account";
+import NewPost from "./Components/NewPost/NewPost";
 import Register from "./Components/Register/Register";
 import UpdateProfile from "./Components/UpdateProfile/UpdateProfile";
 import UpdatePassword from "./Components/UpdatePassword/UpdatePassword";
 import ForgotPassword from "./Components/ForgotPassword/ForgotPassword";
 import ResetPassword from "./Components/ResetPassword/ResetPassword";
-import Home from "./Components/Home/Home";
-import Account from "./Components/Account/Account";
-import NewPost from "./Components/NewPost/NewPost";
 import UserProfile from "./Components/UserProfile/UserProfile";
 import Search from "./Components/Search/Search";
 import NotFound from "./Components/NotFound/NotFound";
+
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -26,9 +27,9 @@ function App() {
 
   return (
     <Router>
-    {isAuthenticated && <Header />}
-   
-    <Routes>
+      {isAuthenticated && <Header />}
+
+      <Routes>
         <Route path="/" element={isAuthenticated ? <Home /> : <Login />} />
         <Route
           path="/account"
@@ -54,12 +55,12 @@ function App() {
           element={isAuthenticated ? <UpdatePassword /> : <Login />}
         />
 
-         <Route
+        <Route
           path="/forgot/password"
           element={isAuthenticated ? <UpdatePassword /> : <ForgotPassword />}
         />
-       
-       <Route
+
+        <Route
           path="/password/reset/:token"
           element={isAuthenticated ? <UpdatePassword /> : <ResetPassword />}
         />
@@ -72,10 +73,9 @@ function App() {
         <Route path="search" element={<Search />} />
 
         <Route path="*" element={<NotFound />} />
-    
-    </Routes>
+      </Routes>
     </Router>
-    
   );
 }
+
 export default App;
